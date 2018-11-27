f4357cf100fab918e045f265330db3dbe6208329
imbafrank <imbafrank@126.com>
2018-11-26 19:38:13 -0500

    Add files via upload

---
+++ agg.v
@@ -0,0 +1,40 @@
+
+// *********************************************************************************
+// Project Name : CSEE 4823 Project: Neural Network Accelerator
+// File Name    : agg.v
+// *********************************************************************************
+// Modification History:
+// Date         By              Version                 Change Description
+// -----------------------------------------------------------------------
+// 11/21/2018 Shixin Qin      1.0                     Basic function design
+// 
+// *********************************************************************************
+
+module agg (clk, rst, agg_in, agg_out2alu, agg_out2act, act_out);
+    parameter n = 12;
+    input               clk, rst;
+    input wire [n-1:0] 		agg_in ;
+    output [n-1:0]      agg_out ;
+    output reg			agg_out2alu, agg_out2act;
+    output 				act_outacted; 
+	wire 				clk, rst;
+	wire					agg;
+	assign agg_lsb = agg_in[0];
+
+    always @(posedge clk, posedge rst)
+    begin
+    	if(rst==1)
+    		begin
+    			agg_out2alu <= 0;
+    		end
+    		
+    	else 
+    	    begin
+    	        agg_out2alu <= agg_in;
+    			agg_out2act <= agg_lsb;
+    	    end
+    	
+    end
+
+    assign act_out_acted = !agg_out2act;
+endmodule
--- alu.v
+++ alu.v
@@ -12,25 +12,50 @@
 // 
 // *********************************************************************************
 
-module alu (rst, alu_in_a_lsb, alu_in_b, alu_op, alu_out);
-    input               	rst, alu_in_a_lsb, alu_op ;
-    input signed [11:0] 	alu_in_b;
-    output reg signed [11:0]	alu_out ;
+module alu (alu_in_a_lsb, alu_in_b, alu_op, alu_out);
+    parameter n = 12;
+    input               		alu_in_a_lsb, alu_op ;
+    input signed [n-1:0] 		alu_in_b;
+    output signed [n-1:0]	alu_out ;
+    wire						alu_in_a_lsb, alu_in_b, alu_op, alu_b;
+    
+    // always @(*)
+    // begin
+    // 	case (alu_op) 
+    // 	    `alu_op_add1:
+    // 	        begin
+    // 	            alu_out = alu_b + alu_in_a_lsb;			
+    // 	        end
+    // 	    `alu_op_sub1:
+    // 	        begin
+    // 	            alu_out = alu_b - alu_in_a_lsb;
+    // 	        end
+    // 	    default:
+    // 	        begin
+    // 	            alu_out = {n{1'b0}}; 
+    // 	        end
+    // 	endcase
+
+    // end
+
+
+    reg [n-1:0]					alu_result;
+    assign alu_out = alu_result;
 
     always @(*)
     begin
     	case (alu_op) 
     	    `alu_op_add1:
     	        begin
-    	            alu_out = alu_in_b + alu_in_a_lsb;			
+    	            alu_result = alu_b + alu_in_a_lsb;			
     	        end
     	    `alu_op_sub1:
     	        begin
-    	            alu_out = alu_in_b - alu_in_a_lsb;
+    	            alu_result = alu_b - alu_in_a_lsb;
     	        end
     	    default:
     	        begin
-    	            alu_out = 8'b0; 
+    	            alu_result = {n{1'b0}}; 
     	        end
     	endcase
 
