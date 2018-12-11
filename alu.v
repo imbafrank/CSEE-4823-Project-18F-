3070e2350312b3ccf79ab0dd2da99b77bf5fca99
imbafrank <imbafrank@126.com>
2018-12-10 20:24:58 -0800

    alu tested

--- alu.v
+++ alu.v
@@ -1,5 +1,5 @@
-`include "ctrl_signal.v"
-
+	`define alu_op_add1   	1'b0
+	`define alu_op_sub1 	1'b1
 // *********************************************************************************
 // Project Name : CSEE 4823 Project: Neural Network Accelerator
 // File Name    : alu.v
@@ -9,15 +9,15 @@
 // Date         By              Version                 Change Description
 // -----------------------------------------------------------------------
 // 11/13/2018 Shixin Qin      1.0                     Basic function design
-// 
+// 12/10/2018 Shixin Qin      1.1                      Tested, q:signed or unsigned?
 // *********************************************************************************
 
 module alu (alu_in_a_lsb, alu_in_b, alu_op, alu_out);
-    parameter n = 12;
+    parameter alu_width = 12;
     input               		alu_in_a_lsb, alu_op ;
-    input signed [n-1:0] 		alu_in_b;
-    output signed [n-1:0]	alu_out ;
-    wire						alu_in_a_lsb, alu_in_b, alu_op, alu_b;
+    input signed[alu_width-1:0] 		alu_in_b;
+    output reg signed[alu_width-1:0]	alu_out ;
+    wire						alu_in_a_lsb, alu_in_b, alu_op;
     
     // always @(*)
     // begin
@@ -39,25 +39,30 @@ module alu (alu_in_a_lsb, alu_in_b, alu_op, alu_out);
     // end
 
 
-    reg [n-1:0]					alu_result;
-    assign alu_out = alu_result;
-
+    //reg 					alu_result;
+    wire [alu_width-1:0] alu_b;
+    assign alu_b = (alu_in_b>0)?alu_in_b:0;
+    
     always @(*)
     begin
+    // assign alu_out =  alu_in_b;	
     	case (alu_op) 
-    	    `alu_op_add1:
+    	    0:
     	        begin
-    	            alu_result = alu_b + alu_in_a_lsb;			
+    	            alu_out = alu_b+alu_in_a_lsb;			
     	        end
-    	    `alu_op_sub1:
+    	    1:
     	        begin
-    	            alu_result = alu_b - alu_in_a_lsb;
+    	            alu_out = alu_b-alu_in_a_lsb;
     	        end
     	    default:
     	        begin
-    	            alu_result = {n{1'b0}}; 
+    	            alu_out = 0; 
     	        end
     	endcase
 
     end
+    
+    
+
 endmodule
--- alu_tb.v
+++ alu_tb.v
@@ -5,41 +5,54 @@ module tb_alu;
 
 // alu Parameters
 parameter PERIOD = 10;
-parameter n  = 12;
+parameter alu_width  = 12;
 
 // alu Inputs
+reg   clk                                  = 0 ;
+reg   rst                                  = 0 ;
 reg   alu_in_a_lsb                         = 0 ;
 reg   alu_op                               = 0 ;
-reg   [n-1:0]  alu_in_b                    = 0 ;
+reg   [alu_width-1:0]  alu_in_b                    = 0 ;
 
 // alu Outputs
-wire  [n-1:0]  alu_out                     ;
+wire  [alu_width-1:0]  alu_out                     ;
 
 
-initial
-begin
-    forever #(PERIOD/2)  clk=~clk;
-end
 
 initial
 begin
-    #(PERIOD*2) rst_n  =  1;
+    #10 rst  =  1;
+    #10 alu_op=0;alu_in_a_lsb=1;alu_in_b=0;
+    #10 alu_op=0;alu_in_a_lsb=1;alu_in_b=1;
+    #10 alu_op=0;alu_in_a_lsb=0;alu_in_b=2;
+    #10 alu_op=0;alu_in_a_lsb=1;alu_in_b=3;
+    #10 alu_op=1;alu_in_a_lsb=1;alu_in_b=2;
+    #10 alu_op=1;alu_in_a_lsb=0;alu_in_b=2;
+    #10 alu_op=1;alu_in_a_lsb=1;alu_in_b=-1;
+    #10 alu_op=1;alu_in_a_lsb=1;alu_in_b=0;
+    #10 alu_op=0;alu_in_a_lsb=1;alu_in_b=4095;
+    
+
 end
 
 alu #(
-    .n ( n ))
+    .alu_width ( alu_width ))
  u_alu (
     .alu_in_a_lsb            ( alu_in_a_lsb          ),
     .alu_op                  ( alu_op                ),
-    .alu_in_b                ( alu_in_b      [n-1:0] ),
+    .alu_in_b                ( alu_in_b      [alu_width-1:0] ),
 
-    .alu_out                 ( alu_out       [n-1:0] )
+    .alu_out                 ( alu_out       [alu_width-1:0] )
 );
 
 initial
 begin
 
-    $finish;
+    $dumpfile("/home/netlab/nna/df.vcd");
+    $dumpvars(0, tb_alu);
+    $display("\t\ttime,\tclk,\treset,\top,\ta,\tb,\tout"); 
+    $monitor("%d,\t%b,\t%b,\t%b,\t%b,\t%b",$time, clk,rst,alu_op, alu_in_a_lsb, alu_in_b,alu_out); 
+    #100$finish;
 end
 
 endmodule
