900e0517be5ebced7ba4e3cb1a5a458a467a947b
imbafrank <imbafrank@126.com>
2018-12-10 17:42:33 -0800

    tb agg, cmp

--- agg_tb.v
+++ agg_tb.v
@@ -5,16 +5,16 @@ module tb_agg;
 
 // agg Parameters
 parameter PERIOD = 10;
-parameter n  = 12;
+parameter agg_width  = 12;
 
 // agg Inputs
 reg   clk                                  = 0 ;
 reg   rst                                  = 0 ;
-reg   [n-1:0]  agg_in                      = 0 ;
+reg   [agg_width-1:0]  agg_in                      = 0 ;
 
 // agg Outputs
-wire  [n-1:0]  agg_out                     ;
-wire  agg_out2alu                          ;
+//wire  [agg_width-1:0]  agg_out                     ;
+wire  [agg_width-1:0] agg_out2alu                         ;
 wire  agg_out2act                          ;
 wire  agg_out_acted                        ;
 
@@ -26,26 +26,34 @@ end
 
 initial
 begin
-    #(PERIOD*2) rst_n  =  1;
+    #(PERIOD*2) rst  =  1;
+    #15 agg_in=1;
+    #15 agg_in=3;
+    #15 agg_in=2;
+    #10 agg_in=1024;
+    #10 agg_in=2048;
+
 end
 
 agg #(
-    .n ( n ))
+    .agg_width ( agg_width ))
  u_agg (
     .clk                     ( clk                    ),
     .rst                     ( rst                    ),
-    .agg_in                  ( agg_in         [n-1:0] ),
+    .agg_in                  ( agg_in         [agg_width-1:0] ),
 
-    .agg_out                 ( agg_out        [n-1:0] ),
-    .agg_out2alu             ( agg_out2alu            ),
+    .agg_out2alu                 ( agg_out2alu        [agg_width-1:0] ),
     .agg_out2act             ( agg_out2act            ),
     .agg_out_acted           ( agg_out_acted          )
 );
 
 initial
 begin
-
-    $finish;
+$dumpfile("/home/netlab/nna/df.vcd");
+    $dumpvars(0, tb_agg);
+     $display("\t\ttime,\tclk,\treset,\tin,\tout2act,\toutacted,\tout2alu"); 
+    $monitor("%d,\t%b,\t%b,\t%b,\t%b,\t%b,\t%b",$time, clk,rst,agg_in,agg_out2act, agg_out_acted, agg_out2alu); 
+    #100$finish;
 end
 
 endmodule
--- cmp_tb.v
+++ cmp_tb.v
@@ -5,15 +5,16 @@ module tb_cmp;
 
 // cmp Parameters
 parameter PERIOD  = 10;
-
+parameter cmp_width = 4;
 
 // cmp Inputs
 reg   clk                                  = 0 ;
 reg   rst                                  = 0 ;
-reg   cmp_in_a                             = 0 ;
+reg  [cmp_width-1:0]   cmp_in_a                      = 0 ;
 
 // cmp Outputs
-wire  cmp_out                              ;
+wire [cmp_width-1:0]  cmp_out                             ;
+wire [cmp_width-1:0]  cmp_b                           ;
 
 
 initial
@@ -23,7 +24,18 @@ end
 
 initial
 begin
-    #(PERIOD*2) rst_n  =  1;
+    #(PERIOD*2) rst  =  1;
+    #15 cmp_in_a=1;
+    #25 cmp_in_a=3;
+    #35 cmp_in_a=2;
+end
+
+initial
+begin
+    $dumpfile("/home/netlab/nna/df.vcd");
+    $dumpvars(0, tb_cmp);
+     $display("\t\ttime,\tclk,\treset,\tin,\tout"); 
+    $monitor("%d,\t%b,\t%b,\t%b,\t%b,\t%b",$time, clk,rst,cmp_in_a,cmp_out, cmp_b); 
 end
 
 cmp  u_cmp (
@@ -31,13 +43,15 @@ cmp  u_cmp (
     .rst                     ( rst        ),
     .cmp_in_a                ( cmp_in_a   ),
 
-    .cmp_out                 ( cmp_out    )
+    .cmp_out                 ( cmp_out    ),
+    .cmp_b                ( cmp_b   )
+
 );
 
 initial
 begin
 
-    $finish;
+    #100$finish;
 end
 
 endmodule
