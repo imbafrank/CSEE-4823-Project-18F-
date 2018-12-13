66a3559dbd73fd859249328070efec6d383e4cb0
David <davidwang2773@gmail.com>
2018-12-13 16:55:02 -0500

    modified contrl unit

--- compute_module.v
+++ compute_module.v
@@ -1,79 +1,111 @@
 `timescale 1ns/1ps
-
 // define state
 `define rest ;
-`define load_input ;
-`define load_weight ;
-`define compute ;
-`define activation ;
-`define store_output ;
+`define load_x1 ;
+`define load_w1 ;
+`define compute_x2
+`define load_x2 ;
+`define load_w2 ;
+`define compute_x3
+`define load_x3 ;
+`define load_w3 ;
+`define comnpute_x4;
+`define load_x4 ;
+`define load_w4 ;
+`define compute_output;
 `define finish ;
 
 module compute_module
 #(
-	parameter W_ADDR_LEN = 17;
-	parameter W_DATA_LEN = 8;
-	parameter I_ADDR_LEN = 10;
-	parameter I_DATA_LEN = 8;
+	parameter W_ADDR_LEN = 20;
+	parameter W_DATA_LEN = 1;
+	parameter W_SEL_LEN = 2;
+	parameter W_RW_LEN = 2;
+	parameter X_ADDR_LEN = 10;
+	parameter X_DATA_LEN = 1;
+	parameter X_SEL_LEN = 2;
+	parameter X_RW_LEN = 2;
 	)
 (
 	// control
 	start_compute,
-	compute_finish
-	// ram
-	// addr_weight,
-	// data_weight,
-	// addr_input,
-	// data_input
+	compute_finish,
+	// connect to mem
+	w_addr,
+	w_data,
+	w_sel,
+	w_rw,
+	x_addr,
+	x_data,
+	x_sel,
+	x_rw
 	);
 
 
+// instantiate weight memory
 weight_ram weight_ram_i
 (
 	.read_write(read_write_weight),
 	.addr_weight(addr_weight),
 	.data_weight(data_weight))
-
+// instantiate input memory
 input_ram input_ram_i
 (
 	.read_write(read_write_input),
 	.addr_input(addr_input),
 	.data_input(data_input))
+// instantiate compute block
 
+ alu_agg_i
+(
+	)
 
 input rst;
 input clk;
 
-// control
-input start_compute;
-reg compute_finish;
+// control signal from tb
+input wire start_compute;
+output reg compute_finish;
 
-// weight ram
-reg read_write_weight;
-reg [W_ADDR_LEN-1:0] addr_weight;
-wire [W_DATA_LEN-1:0] data_weight;
-reg [W_DATA_LEN-1:0] data_weight_reg;
-reg [15:0] load_weight_counter;
-// input ram
-reg read_write_input;
-reg [I_ADDR_LEN-1:0] addr_input;
-wire [I_DATA_LEN-1:0] data_input;
-reg [I_DATA_LEN-1:0] data_input_reg;
-reg [15:0] load_input_counter;
+// signal to mem
+wire [W_ADDR_LEN-1:0] w_addr;
+wire [W_DATA_LEN-1:0] w_data;
+wire [W_SEL_LEN-1:0] w_sel;
+wire [W_RW_LEN-1:0] w_rw;
+wire [X_ADDR_LEN-1:0] x_addr;
+wire [X_DATA_LEN-1:0] x_data;
+wire [X_SEL_LEN-1:0] x_sel;
+wire [X_RW_LEN-1:0] x_rw;
+
+// weight counter
+reg [W_ADDR_LEN-1:0] load_weight_counter;
+reg [W_SEL_LEN-1:0] sel_weight_counter;
+
+// input counter
+reg [X_ADDR_LEN-1:0] load_x_counter;
+reg [X_SEL_LEN-1:0] sel_x_counter
+
+// reg store current value
+reg [W_DATA_LEN-1:0] store_weight_reg;
+reg [X_DATA_LEN-1:0] store_x_reg;
+// reg store current rw status
+reg []
 
 // state
 reg [2:0] state;
-// weight ram control
-reg [W_ADDR_LEN-1:0] addr_weight
 
 // rest state counter
 reg [3:0] rest_counter;
+reg rest_finish
 
-// compute result reg
-reg signed [15:0] result_reg;
 // activation result reg
 reg output_reg;
-reg [1023:0] output_mem;
+
+// connect with mem
+assign w_addr = load_weight_counter;
+assign w_sel = sel_weight_counter;
+assign w_
+
 
 // when rest 10 cycles, go to next state. 
 assign rest_finish = rest_counter>10;
