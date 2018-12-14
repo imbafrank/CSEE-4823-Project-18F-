68633fe2b7d0d7f31a63929d3be3e1f56612ef7a
David <davidwang2773@gmail.com>
2018-12-13 19:54:22 -0500

    updated compute control module

--- compute_module.v
+++ compute_module.v
@@ -1,17 +1,13 @@
 `timescale 1ns/1ps
 // define state
 `define rest ;
-`define load_x1 ;
-`define load_w1 ;
+`define load_1 ;
 `define compute_x2
-`define load_x2 ;
-`define load_w2 ;
+`define load_2 ;
 `define compute_x3
-`define load_x3 ;
-`define load_w3 ;
+`define load_3 ;
 `define comnpute_x4;
-`define load_x4 ;
-`define load_w4 ;
+`define load_4 ;
 `define compute_output;
 `define finish ;
 
@@ -25,8 +21,12 @@ module compute_module
 	parameter X_DATA_LEN = 1;
 	parameter X_SEL_LEN = 2;
 	parameter X_RW_LEN = 2;
+
+	parameter alu_width  = 12;
 	)
 (
+	clk,
+	rst,
 	// control
 	start_compute,
 	compute_finish,
@@ -41,24 +41,15 @@ module compute_module
 	x_rw
 	);
 
-
-// instantiate weight memory
-weight_ram weight_ram_i
-(
-	.read_write(read_write_weight),
-	.addr_weight(addr_weight),
-	.data_weight(data_weight))
-// instantiate input memory
-input_ram input_ram_i
-(
-	.read_write(read_write_input),
-	.addr_input(addr_input),
-	.data_input(data_input))
 // instantiate compute block
-
- alu_agg_i
+ calc calc_i
 (
-	)
+	.clk(clk),
+	.rst(rst),
+	.calc_1(calc_1),
+	.calc_in(calc_in),
+	.agg_out2alu(agg_out2alu),
+	.agg_out_acted(agg_out_acted))
 
 input rst;
 input clk;
@@ -77,6 +68,12 @@ wire [X_DATA_LEN-1:0] x_data;
 wire [X_SEL_LEN-1:0] x_sel;
 wire [X_RW_LEN-1:0] x_rw;
 
+// signal to calc
+wire calc_1;
+wire calc_in;
+wire [alu_width-1:0] agg_out2alu;
+wire agg_out_acted;
+
 // weight counter
 reg [W_ADDR_LEN-1:0] load_weight_counter;
 reg [W_SEL_LEN-1:0] sel_weight_counter;
@@ -89,14 +86,15 @@ reg [X_SEL_LEN-1:0] sel_x_counter
 reg [W_DATA_LEN-1:0] store_weight_reg;
 reg [X_DATA_LEN-1:0] store_x_reg;
 // reg store current rw status
-reg []
+reg [W_RW_LEN-1:0] rw_w_reg;
+reg [X_RW_LEN-1:0] rw_x_reg;
 
 // state
 reg [2:0] state;
 
 // rest state counter
 reg [3:0] rest_counter;
-reg rest_finish
+reg rest_finish;
 
 // activation result reg
 reg output_reg;
@@ -104,8 +102,12 @@ reg output_reg;
 // connect with mem
 assign w_addr = load_weight_counter;
 assign w_sel = sel_weight_counter;
-assign w_
-
+// assign w_data = store_weight_reg;
+assign w_rw = rw_w_reg;
+assign x_addr = load_x_counter;
+// assign x_data = store_x_reg;
+assign x_sel = sel_x_counter;
+assign x_rw = rw_x_reg;
 
 // when rest 10 cycles, go to next state. 
 assign rest_finish = rest_counter>10;
@@ -123,7 +125,7 @@ always @(posedge clk or posedge rst) begin
 		case (state)
 		`rest:	begin
 			if (rest_finish==1) begin
-				state <= `load_input;
+				state <= `load_x1;
 				rest_counter <= 0;
 			end
 			else begin
@@ -132,34 +134,31 @@ always @(posedge clk or posedge rst) begin
 			end
 		end
 
-		`load_input: begin
-			data_input_reg <= data_input;
-			result_reg <= 0;
-			state <= `load_weight
-		end
+		`load_1: begin
+			sel_weight_counter <= 0;
+			sel_x_counter <= 0;
 
-		`load_weight: begin
-			data_weight_reg <= data_weight;
+			store_weight_reg <= w_data;
+			store_x_reg <= x_data;
+
 			load_weight_counter <= load_weight_counter + 1;
-			state <= `compute;
+			load_x_counter <= load_x_counter + 1;
+
+			if (load_weight_counter >= 802816 and load_x_counter >= 784) begin
+				state <= `compute_x2;
+			end
+
+			if (load_x_counter >= 784) begin
+				load_x_counter <= 0;
+			end
+
+			if (load_weight_counter >= 802816) begin
+				load_weight_counter <= 0;
+			end
 		end
 
 		`compute: begin
-			if (data_weight_reg == 0) begin
-				result_reg <= result_reg - 1;
-			end
-			else if (data_weight_reg == 1) begin
-				result_reg <= result_reg + 1;
-			end
 
-			if (load_weight_counter == 1024) begin
-				state <= `activation;
-				load_weight_counter <= 0;
-			end
-			else begin
-				state <= `load_weight;
-				addr_weight <= addr_weight + 1;
-			end
 
 		end
 
