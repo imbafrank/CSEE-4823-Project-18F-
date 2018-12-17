ecd1dc44af86bba2a676de40869315f9e3c04320
imbafrank <imbafrank@126.com>
2018-12-16 20:20:55 -0500

    mod

--- lfsr1.v
+++ lfsr1.v
@@ -4,23 +4,23 @@
 //
 `timescale 1ns/1ps
 
-module lfsr1 (clk, i,j,write_enable,din,dout);
+module lfsr1 (
 input clk,
 	input wire [9:0] i,
 	input wire [8:0] j,
-	input write_enalbe,
+	input write_enable,
 	input wire [7:0] din,
 	output reg [7:0] dout
 	);
 
-	reg 7:0] M[0:307199];
+	reg [7:0] M[0:307199];
 	wire [18:0] addr;
 	
 	assign addr = i*640 + j;
 
 
 	always @(posedge clk) begin
-	if (write_enable = 1'b1)
+	if (write_enable == 1'b1)
 		M[addr] <= din;
 	dout <= M[addr];
 	end
